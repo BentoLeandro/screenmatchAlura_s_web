16e2146057d72fced8898ded1d7d8f6d53d4b93d
leandro <leo_lrb90@hotmail.com>
2024-10-22 15:46:26 -0300

    Melhorias na organização do Código

--- src/main/java/br/com/rbcontrole/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/rbcontrole/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.rbcontrole.screenmatch;
 
-import br.com.rbcontrole.screenmatch.model.DadosSerie;
-import br.com.rbcontrole.screenmatch.service.ConsumoApi;
-import br.com.rbcontrole.screenmatch.service.ConverteDados;
+import br.com.rbcontrole.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,13 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=994fd5f4");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
